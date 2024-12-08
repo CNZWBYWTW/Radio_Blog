9d471f7a5ecbcf73269e06f70a0b4d7d1e2f9961
CNZW <2429671588@qq.com>
2024-12-08 15:07:24 +0800

    JavaScript

--- js/all.js
+++ js/all.js
@@ -1,3 +1,10 @@
+//////////////////////////////
+alert(`此站处于待运行(未正式启用)状态\n\n当前时间: ${new Date().toUTCString()}\n预计正式启用时间: 2025-07-01 ~ 2025-09-01\n\nPowered by CNZW\nhttps://cnzw.us.kg/\nGitHub@CNZWBYWTW`);
+//////////////////////////////
+
+
+
+//CNZW FUNCTIONS
 function getURL(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
