14252b4708fc2f420c3c6c06241b4a4ad3fe8331
CNZW <2429671588@qq.com>
2024-12-08 22:42:59 +0800

    JavaScript

--- js/all.js
+++ js/all.js
@@ -1,5 +1,5 @@
 //////////////////////////////
-window.onload = function(){alert(`此站处于待运行(未正式启用)状态\n\n当前时间: ${new Date().toUTCString().replace('GMT', 'UTC')}\n预计正式启用时间: 2025-07-01 ~ 2025-09-01\n\nPowered by CNZW\nhttps://cnzw.us.kg/\nGitHub@CNZWBYWTW`);}
+alert(`此站处于待运行(未正式启用)状态\n\n当前时间: ${new Date().toUTCString().replace('GMT', 'UTC')}\n预计正式启用时间: 2025-07-01 ~ 2025-09-01\n\nPowered by CNZW\nhttps://cnzw.us.kg/\nGitHub@CNZWBYWTW`);
 //////////////////////////////
 
 
